2093363b73f723963cea21aed833976d34e5db12
Nilay Deshmukh <60890829+nilaydeshmukh0@users.noreply.github.com>
2020-06-03 14:05:29 +0530

    Add files via upload

---
+++ Fireforests Analysis/Fireforests Analysis.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 15:54:25 2020
+@author: DESHMUKH
+Support Vector Machine 
+"""
+# pip install keras
+# pip install tensorflow
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+import seaborn as sns
+import scipy.stats as stats
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
+from sklearn.preprocessing import normalize
+pd.set_option('display.max_columns',None)
+
+# =================================================================================================================
+# Business Problem - Prepare support vector machines model for classifying the area under fire for foresfires data.
+# =================================================================================================================
+
+ff = pd.read_csv('fireforests.csv')
+ff.head()
+ff = ff.iloc[:,2:]
+ff.info()
+ff.isnull().sum()
+
+# Summary
+ff.describe()
+
+# Histogram
+ff.hist(grid = False)
+
+# Boxplot
+ff.boxplot(notch = True, patch_artist = True, grid = False);plt.xticks(fontsize=6, rotation = 90)
+
+# Pairplot
+sns.pairplot(ff.iloc[:,0:9], corner = True, diag_kind = "kde")
+
+# Heat map and Correlation Coifficient 
+sns.heatmap(ff.iloc[:,0:9].corr(), annot = True, annot_kws={"size": 6}, cmap = 'Oranges')
+
+# WE CAN OBSERVE FROM ABOVE HEATMAP THERE IS NO CORRELATION IN BETWEEN OUTPUT AND INPUT FEATURES.
+# SO WE CAN'T ABLE TO GET PROPER RESULT WITH HELP OF THIS INPUT FEATURE ONLY. 
+
+###################################### - Data Preprocessing - #######################################
+
+# Nomalization of data (as data contain binary value)
+ff.iloc[:,0:8] = normalize(ff.iloc[:,0:8])
+
+##################################### - Splitting data - ############################################
+
+# Splitting in X and y
+X = ff.drop(['area'],axis=1)
+y = ff['area']
+
+# Splitting in Train and Test 
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 0.3)
+
+######################################### - Fitting Model- ##########################################
+
+# Fitting SVM Model
+model = SVR(kernel = 'rbf', gamma = 1, epsilon = 0.01)
+model.fit(X_train,y_train)
+      
+# Accuracy of Model on Train and Test data
+model.score(X_train,y_train)
+model.score(X_test,y_test)
+
+# Predication Train data and Test data
+pred_train = model.predict(X_train)
+pred_test = model.predict(X_test)
+
+# RMSE Train data and Test data
+rmse_train = np.sqrt(np.mean((pred_train-y_train)**2))
+rmse_test = np.sqrt(np.mean((pred_test-y_test)**2)) 
+
+# Visualising Train data and Test data
+plt.plot(pred_train,y_train,"bo")
+plt.plot(pred_test,y_test,"ro")
+
+                                # ---------------------------------------------------- #
---
+++ Salary Data Analysis/Salary Data Analysis.py
@@ -0,0 +1,91 @@
+"""
+Created on Wed May 13 12:18:18 2020
+@author: DESHMUKH
+Support Vector Machine
+"""
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+
+pd.set_option('display.max_columns',None)
+
+# ===============================================================================================
+# Business Problem - Prepare a classification model using SVM for salary data.
+# ===============================================================================================
+
+salary_train = pd.read_csv("SalaryData_Train.csv")
+salary_test = pd.read_csv("SalaryData_Test.csv")
+salary_train.info()
+salary_test.info()
+salary_test.head()
+
+# Value counts
+salary_train.groupby('workclass').size()
+salary_train.groupby('education').size()
+salary_train.groupby('maritalstatus').size()
+salary_train.groupby('occupation').size()
+salary_train.groupby('relationship').size()
+salary_train.groupby('race').size()
+salary_train.groupby('sex').size()
+salary_train.groupby('native').size()
+
+############################ - Converting input variable into LabelEncoder - ############################
+
+# All input categorical columns
+string_columns=["workclass","education","maritalstatus","occupation","relationship","race","sex","native"]
+
+# Counverting into Numerical Data by means of custome function and Lable Encoder
+for i in string_columns:
+    number = LabelEncoder()
+    salary_train[i] = number.fit_transform(salary_train[i])
+    salary_test[i] = number.fit_transform(salary_test[i])
+
+######################################### - Data Preprocessing - ########################################
+
+from sklearn.preprocessing import scale
+# Nomalization of data (as data contain binary value)
+salary_train.iloc[:,0:13] = scale(salary_train.iloc[:,0:13])
+salary_test.iloc[:,0:13] = scale(salary_test.iloc[:,0:13])
+
+##################################### - Spliting data in X and y - ######################################
+
+X_train = salary_train.iloc[:,0:13]
+y_train = salary_train.iloc[:,13]
+X_test  = salary_test.iloc[:,0:13]
+y_test  = salary_test.iloc[:,13]
+
+########################################### - Fitting Model - ###########################################
+# Kernel option - 'linear', 'poly', 'rbf', 'sigmoid'
+
+# Fitting SVM Model
+model = SVC(kernel = "linear")
+model.fit(X_train,y_train)
+
+# Accuracy
+model.score(X_train,y_train) 
+model.score(X_test,y_test) 
+
+# Prediction on Train & Test Data
+pred_train = model.predict(X_train)
+pred_test = model.predict(X_test)
+
+# Confusion matrix of Train and Test
+## Train
+confusion_matrix_train = pd.crosstab(y_train,pred_train,rownames=['Actual'],colnames= ['Train Predictions']) 
+sns.heatmap(confusion_matrix_train, annot = True, cmap = 'Blues',fmt='g')
+
+## Test
+confusion_matrix_test = pd.crosstab(y_test,pred_test,rownames=['Actual'],colnames= ['Test Predictions']) 
+sns.heatmap(confusion_matrix_test, annot = True, cmap = 'Greens',fmt='g')
+
+# Classification Report of test
+print(classification_report(y_test,pred_test))
+
+
+                          # ---------------------------------------------------- #
